d70e88ac3debc47113432b7f1e923f4aed37f9f6
zhucr17194 <zhucr17194@hundsun.com>
2024-12-04 10:00:34 +0800

    init

---
+++ BoundaryHighlighter/BoundaryHighlighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoundaryHighlighterApp.swift
+//  BoundaryHighlighter
+//
+//  Created by zcr on 2024/12/4.
+//
+
+import SwiftUI
+
+@main
+struct BoundaryHighlighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
